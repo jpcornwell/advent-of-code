e2f95c205f2e9efe66f97983b58c83c0e9dbda5d
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-02-20 21:25:02 -0600

    Do day 5

--- 1-2015/05-Day-Doesnt_He_Have_Intern_Elves_For_This/py/1-part.py
+++ 1-2015/05-Day-Doesnt_He_Have_Intern_Elves_For_This/py/1-part.py
@@ -1,1 +1,22 @@
-Placeholder
+import re
+
+with open('../input.txt') as f:
+    content = f.readlines()
+
+nice_count = 0
+for line in content:
+    is_nice = True
+
+    if sum([line.count(x) for x in 'aeiou']) < 3:
+        is_nice = False
+
+    if not re.search(r'(.)\1', line):
+        is_nice = False
+
+    if 'ab' in line or 'cd' in line or 'pq' in line or 'xy' in line:
+        is_nice = False
+
+    if is_nice:
+        nice_count += 1
+
+print(nice_count)
--- 1-2015/05-Day-Doesnt_He_Have_Intern_Elves_For_This/py/2-part.py
+++ 1-2015/05-Day-Doesnt_He_Have_Intern_Elves_For_This/py/2-part.py
@@ -1,1 +1,19 @@
-Placeholder
+import re
+
+with open('../input.txt') as f:
+    content = f.readlines()
+
+nice_count = 0
+for line in content:
+    is_nice = True
+
+    if not re.search(r'(..).*\1', line):
+        is_nice = False
+
+    if not re.search(r'(.).\1', line):
+        is_nice = False
+
+    if is_nice:
+        nice_count += 1
+
+print(nice_count)
