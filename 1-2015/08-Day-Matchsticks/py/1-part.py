6f910aa49c751441d5a30629b75ccbea169aa1e1
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-03-03 23:52:49 -0600

    Do day 8

--- 1-2015/08-Day-Matchsticks/py/1-part.py
+++ 1-2015/08-Day-Matchsticks/py/1-part.py
@@ -1,1 +1,17 @@
-Placeholder
+import re
+
+with open('../input.txt') as f:
+    content = f.read().splitlines()
+
+literal_char_count = 0
+for line in content:
+    literal_char_count += len(line)
+
+memory_char_count = 0
+for line in content:
+    line = line.replace(r'\\', 'Z')
+    line = line.replace(r'\"', 'Z')
+    line = re.sub(r'\\x[0-9a-fA-F]{2}', 'Z', line)
+    memory_char_count += len(line) - 2 # minus 2 to account for the double quotes
+
+print(literal_char_count - memory_char_count)
--- 1-2015/08-Day-Matchsticks/py/2-part.py
+++ 1-2015/08-Day-Matchsticks/py/2-part.py
@@ -1,1 +1,16 @@
-Placeholder
+import re
+
+with open('../input.txt') as f:
+    content = f.read().splitlines()
+
+literal_char_count = 0
+for line in content:
+    literal_char_count += len(line)
+
+new_char_count = 0
+for line in content:
+    line = line.replace('\\', 'ZZ')
+    line = line.replace(r'"', 'ZZ')
+    new_char_count += len(line) + 2 # plus 2 to account for the double quotes
+
+print(new_char_count - literal_char_count)
