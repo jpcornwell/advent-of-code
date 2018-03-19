9d1a3c4dd3701170931479e772d51ae47df52bd3
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-03-18 20:54:18 -0500

    Do day 10

--- 1-2015/10-Day-Elves_Look_Elves_Say/py/1-part.py
+++ 1-2015/10-Day-Elves_Look_Elves_Say/py/1-part.py
@@ -1,1 +1,15 @@
-Placeholder
+#!/usr/bin/python3
+
+from itertools import groupby
+
+def look_and_say(seq):
+    next_seq = ''.join([str(len(list(g))) + k for k, g in groupby(seq)])
+    return next_seq
+
+with open('../input.txt') as f:
+    seq = f.read().strip()
+
+for _ in range(40):
+    seq = look_and_say(seq)
+
+print(len(seq))
