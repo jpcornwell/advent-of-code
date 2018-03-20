5f1868f408d8a71c4a8ed93871728d555632a807
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-03-19 20:50:59 -0500

    Do day 12

--- 1-2015/12-Day-JSAbacusFramework.io/py/1-part.py
+++ 1-2015/12-Day-JSAbacusFramework.io/py/1-part.py
@@ -1,1 +1,14 @@
-Placeholder
+#!/usr/bin/python3
+
+import re
+
+with open('../input.txt') as f:
+    content = f.read()
+
+numbers = re.findall(r'-?\d+', content)
+
+total = 0
+for number in numbers:
+    total += int(number)
+
+print(total)
--- 1-2015/12-Day-JSAbacusFramework.io/py/2-part.py
+++ 1-2015/12-Day-JSAbacusFramework.io/py/2-part.py
@@ -1,1 +1,25 @@
-Placeholder
+#!/usr/bin/python3
+
+import json
+
+def sumObj(obj):
+    if isinstance(obj, int):
+        return obj
+    if isinstance(obj, str):
+        return 0
+    if isinstance(obj, list):
+        return sum([sumObj(x) for x in obj])
+    if isinstance(obj, dict):
+        if 'red' in obj.values():
+            return 0
+        else:
+            return sum([sumObj(x) for x in obj.values()])
+
+with open('../input.txt') as f:
+    content = f.read()
+
+data = json.loads(content)
+
+total = sumObj(data)
+
+print(total)
