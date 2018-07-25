39ac7cccf85294a2c697c71cd0fcb84f44fe028e
Jacob Cornwell <jacob.p.cornwell@gmail.com>
2018-07-24 23:04:58 -0500

    Solve part 1 day 13

--- 1-2015/13-Day-Knights_of_the_Dinner_Table/py/1-part.py
+++ 1-2015/13-Day-Knights_of_the_Dinner_Table/py/1-part.py
@@ -1,1 +1,42 @@
-Placeholder
+#!/usr/bin/env python
+
+from collections import defaultdict
+from itertools import permutations
+
+graph = defaultdict(dict)
+
+with open('../input.txt') as f:
+    content = f.readlines()
+
+for line in content:
+    (source, _, direction, change, _, _, _, _, _, _, dest) = line.split(' ')
+    dest = dest[:-2] # trim the punctuation and newline
+    change = int(change)
+    if direction == 'lose':
+        change = change * -1
+    graph[source][dest] = change
+
+people = graph.keys()
+perms = list(permutations(people))
+
+max_happy = 0
+for perm in perms:
+    happy = 0
+
+    # handle the first person
+    happy += graph[perm[0]][perm[1]]
+    happy += graph[perm[0]][perm[-1]]
+
+    # handle the last person
+    happy += graph[perm[-1]][perm[-2]]
+    happy += graph[perm[-1]][perm[0]]
+
+    # handle the rest of the people (hard coded to 8 people)
+    for i in range(1, 7):
+        happy += graph[perm[i]][perm[i+1]]
+        happy += graph[perm[i]][perm[i-1]]
+
+    if happy > max_happy:
+        max_happy = happy
+
+print(max_happy)
